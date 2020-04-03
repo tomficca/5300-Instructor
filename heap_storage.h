db236905272e153ec22dbabe54ed1181537ac419
Kevin Lundeen <kevin.lundeen@gmail.com>
2020-04-02 17:59:19 -0700

    final rev to satisfy Milestone1

--- heap_storage.h
+++ heap_storage.h
@@ -29,32 +29,47 @@
  */
 class SlottedPage : public DbBlock {
 public:
-	SlottedPage(Dbt &block, BlockID block_id, bool is_new=false);
-	// Big 5 - we only need the destructor, copy-ctor, move-ctor, and op= are unnecessary
-	// but we delete them explicitly just to make sure we don't use them accidentally
-	virtual ~SlottedPage() {}
-	SlottedPage(const SlottedPage& other) = delete;
-	SlottedPage(SlottedPage&& temp) = delete;
-	SlottedPage& operator=(const SlottedPage& other) = delete;
-	SlottedPage& operator=(SlottedPage& temp) = delete;
+    SlottedPage(Dbt &block, BlockID block_id, bool is_new = false);
 
-	virtual RecordID add(const Dbt* data);
-	virtual Dbt* get(RecordID record_id);
-	virtual void put(RecordID record_id, const Dbt &data);
-	virtual void del(RecordID record_id);
-	virtual RecordIDs* ids(void);
+    // Big 5 - we only need the destructor, copy-ctor, move-ctor, and op= are unnecessary
+    // but we delete them explicitly just to make sure we don't use them accidentally
+    virtual ~SlottedPage() {}
+
+    SlottedPage(const SlottedPage &other) = delete;
+
+    SlottedPage(SlottedPage &&temp) = delete;
+
+    SlottedPage &operator=(const SlottedPage &other) = delete;
+
+    SlottedPage &operator=(SlottedPage &temp) = delete;
+
+    virtual RecordID add(const Dbt *data);
+
+    virtual Dbt *get(RecordID record_id);
+
+    virtual void put(RecordID record_id, const Dbt &data);
+
+    virtual void del(RecordID record_id);
+
+    virtual RecordIDs *ids(void);
 
 protected:
-	u_int16_t num_records;
-	u_int16_t end_free;
+    u_int16_t num_records;
+    u_int16_t end_free;
 
-	virtual void get_header(u_int16_t &size, u_int16_t &loc, RecordID id=0);
-	virtual void put_header(RecordID id=0, u_int16_t size=0, u_int16_t loc=0);
-	virtual bool has_room(u_int16_t size);
-	virtual void slide(u_int16_t start, u_int16_t end);
-	virtual u_int16_t get_n(u_int16_t offset);
-	virtual void put_n(u_int16_t offset, u_int16_t n);
-	virtual void* address(u_int16_t offset);
+    virtual void get_header(u_int16_t &size, u_int16_t &loc, RecordID id = 0);
+
+    virtual void put_header(RecordID id = 0, u_int16_t size = 0, u_int16_t loc = 0);
+
+    virtual bool has_room(u_int16_t size);
+
+    virtual void slide(u_int16_t start, u_int16_t end);
+
+    virtual u_int16_t get_n(u_int16_t offset);
+
+    virtual void put_n(u_int16_t offset, u_int16_t n);
+
+    virtual void *address(u_int16_t offset);
 };
 
 /**
@@ -67,30 +82,43 @@ protected:
  */
 class HeapFile : public DbFile {
 public:
-	HeapFile(std::string name) : DbFile(name), dbfilename(""), last(0), closed(true), db(_DB_ENV, 0) {}
-	virtual ~HeapFile() {}
-	HeapFile(const HeapFile& other) = delete;
-	HeapFile(HeapFile&& temp) = delete;
-	HeapFile& operator=(const HeapFile& other) = delete;
-	HeapFile& operator=(HeapFile&& temp) = delete;
+    HeapFile(std::string name) : DbFile(name), dbfilename(""), last(0), closed(true), db(_DB_ENV, 0) {}
 
-	virtual void create(void);
-	virtual void drop(void);
-	virtual void open(void);
-	virtual void close(void);
-	virtual SlottedPage* get_new(void);
-	virtual SlottedPage* get(BlockID block_id);
-	virtual void put(DbBlock* block);
-	virtual BlockIDs* block_ids();
+    virtual ~HeapFile() {}
 
-	virtual u_int32_t get_last_block_id() {return last;}
+    HeapFile(const HeapFile &other) = delete;
+
+    HeapFile(HeapFile &&temp) = delete;
+
+    HeapFile &operator=(const HeapFile &other) = delete;
+
+    HeapFile &operator=(HeapFile &&temp) = delete;
+
+    virtual void create(void);
+
+    virtual void drop(void);
+
+    virtual void open(void);
+
+    virtual void close(void);
+
+    virtual SlottedPage *get_new(void);
+
+    virtual SlottedPage *get(BlockID block_id);
+
+    virtual void put(DbBlock *block);
+
+    virtual BlockIDs *block_ids();
+
+    virtual u_int32_t get_last_block_id() { return last; }
 
 protected:
-	std::string dbfilename;
-	u_int32_t last;
-	bool closed;
-	Db db;
-	virtual void db_open(uint flags=0);
+    std::string dbfilename;
+    u_int32_t last;
+    bool closed;
+    Db db;
+
+    virtual void db_open(uint flags = 0);
 };
 
 /**
@@ -99,35 +127,52 @@ protected:
 
 class HeapTable : public DbRelation {
 public:
-	HeapTable(Identifier table_name, ColumnNames column_names, ColumnAttributes column_attributes );
-	virtual ~HeapTable() {}
-	HeapTable(const HeapTable& other) = delete;
-	HeapTable(HeapTable&& temp) = delete;
-	HeapTable& operator=(const HeapTable& other) = delete;
-	HeapTable& operator=(HeapTable&& temp) = delete;
+    HeapTable(Identifier table_name, ColumnNames column_names, ColumnAttributes column_attributes);
 
-	virtual void create();
-	virtual void create_if_not_exists();
-	virtual void drop();
+    virtual ~HeapTable() {}
 
-	virtual void open();
-	virtual void close();
+    HeapTable(const HeapTable &other) = delete;
 
-	virtual Handle insert(const ValueDict* row);
-	virtual void update(const Handle handle, const ValueDict* new_values);
-	virtual void del(const Handle handle);
+    HeapTable(HeapTable &&temp) = delete;
 
-	virtual Handles* select();
-	virtual Handles* select(const ValueDict* where);
-	virtual ValueDict* project(Handle handle);
-	virtual ValueDict* project(Handle handle, const ColumnNames* column_names);
+    HeapTable &operator=(const HeapTable &other) = delete;
+
+    HeapTable &operator=(HeapTable &&temp) = delete;
+
+    virtual void create();
+
+    virtual void create_if_not_exists();
+
+    virtual void drop();
+
+    virtual void open();
+
+    virtual void close();
+
+    virtual Handle insert(const ValueDict *row);
+
+    virtual void update(const Handle handle, const ValueDict *new_values);
+
+    virtual void del(const Handle handle);
+
+    virtual Handles *select();
+
+    virtual Handles *select(const ValueDict *where);
+
+    virtual ValueDict *project(Handle handle);
+
+    virtual ValueDict *project(Handle handle, const ColumnNames *column_names);
 
 protected:
-	HeapFile file;
-	virtual ValueDict* validate(const ValueDict* row);
-	virtual Handle append(const ValueDict* row);
-	virtual Dbt* marshal(const ValueDict* row);
-	virtual ValueDict* unmarshal(Dbt* data);
+    HeapFile file;
+
+    virtual ValueDict *validate(const ValueDict *row);
+
+    virtual Handle append(const ValueDict *row);
+
+    virtual Dbt *marshal(const ValueDict *row);
+
+    virtual ValueDict *unmarshal(Dbt *data);
 };
 
 bool test_heap_storage();
--- sql5300.cpp
+++ sql5300.cpp
@@ -13,16 +13,17 @@
 #include "SQLParser.h"
 #include "sqlhelper.h"
 #include "heap_storage.h"
+
 using namespace std;
 using namespace hsql;
 
 /*
  * we allocate and initialize the _DB_ENV global
  */
-DbEnv* _DB_ENV;
+DbEnv *_DB_ENV;
 
 // forward declare
-string operatorExpressionToString(const Expr* expr);
+string operatorExpressionToString(const Expr *expr);
 
 /**
  * Convert the hyrise Expr AST back into the equivalent SQL
@@ -30,36 +31,36 @@ string operatorExpressionToString(const Expr* expr);
  * @return     SQL equivalent to *expr
  */
 string expressionToString(const Expr *expr) {
-	string ret;
-	switch (expr->type) {
-	case kExprStar:
-		ret += "*";
-		break;
-	case kExprColumnRef:
-		if (expr->table != NULL)
-			ret += string(expr->table) + ".";
-	case kExprLiteralString:
-		ret += expr->name;
-		break;
-	case kExprLiteralFloat:
-		ret += to_string(expr->fval);
-		break;
-	case kExprLiteralInt:
-		ret += to_string(expr->ival);
-		break;
-	case kExprFunctionRef:
-		ret += string(expr->name) + "?" + expr->expr->name;
-		break;
-	case kExprOperator:
-		ret += operatorExpressionToString(expr);
-		break;
-	default:
-		ret += "???";  // in case there are exprssion types we don't know about here
-		break;
-	}
-	if (expr->alias != NULL)
-		ret += string(" AS ") + expr->alias;
-	return ret;
+    string ret;
+    switch (expr->type) {
+        case kExprStar:
+            ret += "*";
+            break;
+        case kExprColumnRef:
+            if (expr->table != NULL)
+                ret += string(expr->table) + ".";
+        case kExprLiteralString:
+            ret += expr->name;
+            break;
+        case kExprLiteralFloat:
+            ret += to_string(expr->fval);
+            break;
+        case kExprLiteralInt:
+            ret += to_string(expr->ival);
+            break;
+        case kExprFunctionRef:
+            ret += string(expr->name) + "?" + expr->expr->name;
+            break;
+        case kExprOperator:
+            ret += operatorExpressionToString(expr);
+            break;
+        default:
+            ret += "???";  // in case there are exprssion types we don't know about here
+            break;
+    }
+    if (expr->alias != NULL)
+        ret += string(" AS ") + expr->alias;
+    return ret;
 }
 
 /**
@@ -67,37 +68,37 @@ string expressionToString(const Expr *expr) {
  * @param expr operator expression to unparse
  * @return     SQL equivalent to *expr
  */
-string operatorExpressionToString(const Expr* expr) {
-	if (expr == NULL)
-		return "null";
+string operatorExpressionToString(const Expr *expr) {
+    if (expr == NULL)
+        return "null";
 
-	string ret;
-	// Unary prefix operator: NOT
-	if(expr->opType == Expr::NOT)
-		ret += "NOT ";
+    string ret;
+    // Unary prefix operator: NOT
+    if (expr->opType == Expr::NOT)
+        ret += "NOT ";
 
-	// Left-hand side of expression
-	ret += expressionToString(expr->expr) + " ";
+    // Left-hand side of expression
+    ret += expressionToString(expr->expr) + " ";
 
-	// Operator itself
-	switch (expr->opType) {
-	case Expr::SIMPLE_OP:
-		ret += expr->opChar;
-		break;
-	case Expr::AND:
-		ret += "AND";
-		break;
-	case Expr::OR:
-		ret += "OR";
-		break;
-	default:
-		break; // e.g., for NOT
-	}
+    // Operator itself
+    switch (expr->opType) {
+        case Expr::SIMPLE_OP:
+            ret += expr->opChar;
+            break;
+        case Expr::AND:
+            ret += "AND";
+            break;
+        case Expr::OR:
+            ret += "OR";
+            break;
+        default:
+            break; // e.g., for NOT
+    }
 
-	// Right-hand side of expression (only present for binary operators)
-	if (expr->expr2 != NULL)
-		ret += " " + expressionToString(expr->expr2);
-	return ret;
+    // Right-hand side of expression (only present for binary operators)
+    if (expr->expr2 != NULL)
+        ret += " " + expressionToString(expr->expr2);
+    return ret;
 }
 
 /**
@@ -106,51 +107,51 @@ string operatorExpressionToString(const Expr* expr) {
  * @return       SQL equivalent to *table
  */
 string tableRefInfoToString(const TableRef *table) {
-	string ret;
-	switch (table->type) {
-	case kTableSelect:
-		ret += "kTableSelect FIXME"; // FIXME
-		break;
-	case kTableName:
-		ret += table->name;
-		if (table->alias != NULL)
-			ret += string(" AS ") + table->alias;
-		break;
-	case kTableJoin:
-		ret += tableRefInfoToString(table->join->left);
-		switch (table->join->type) {
-		case kJoinCross:
-		case kJoinInner:
-			ret += " JOIN ";
-			break;
-		case kJoinOuter:
-		case kJoinLeftOuter:
-		case kJoinLeft:
-			ret += " LEFT JOIN ";
-			break;
-		case kJoinRightOuter:
-		case kJoinRight:
-			ret += " RIGHT JOIN ";
-			break;
-		case kJoinNatural:
-			ret += " NATURAL JOIN ";
-			break;
-		}
-		ret += tableRefInfoToString(table->join->right);
-		if (table->join->condition != NULL)
-			ret += " ON " + expressionToString(table->join->condition);
-		break;
-	case kTableCrossProduct:
-		bool doComma = false;
-		for (TableRef* tbl : *table->list) {
-			if (doComma)
-				ret += ", ";
-			ret += tableRefInfoToString(tbl);
-			doComma = true;
-		}
-		break;
-	}
-	return ret;
+    string ret;
+    switch (table->type) {
+        case kTableSelect:
+            ret += "kTableSelect FIXME"; // FIXME
+            break;
+        case kTableName:
+            ret += table->name;
+            if (table->alias != NULL)
+                ret += string(" AS ") + table->alias;
+            break;
+        case kTableJoin:
+            ret += tableRefInfoToString(table->join->left);
+            switch (table->join->type) {
+                case kJoinCross:
+                case kJoinInner:
+                    ret += " JOIN ";
+                    break;
+                case kJoinOuter:
+                case kJoinLeftOuter:
+                case kJoinLeft:
+                    ret += " LEFT JOIN ";
+                    break;
+                case kJoinRightOuter:
+                case kJoinRight:
+                    ret += " RIGHT JOIN ";
+                    break;
+                case kJoinNatural:
+                    ret += " NATURAL JOIN ";
+                    break;
+            }
+            ret += tableRefInfoToString(table->join->right);
+            if (table->join->condition != NULL)
+                ret += " ON " + expressionToString(table->join->condition);
+            break;
+        case kTableCrossProduct:
+            bool doComma = false;
+            for (TableRef *tbl : *table->list) {
+                if (doComma)
+                    ret += ", ";
+                ret += tableRefInfoToString(tbl);
+                doComma = true;
+            }
+            break;
+    }
+    return ret;
 }
 
 /**
@@ -159,22 +160,22 @@ string tableRefInfoToString(const TableRef *table) {
  * @return     SQL equivalent to *col
  */
 string columnDefinitionToString(const ColumnDefinition *col) {
-	string ret(col->name);
-	switch(col->type) {
-	case ColumnDefinition::DOUBLE:
-		ret += " DOUBLE";
-		break;
-	case ColumnDefinition::INT:
-		ret += " INT";
-		break;
-	case ColumnDefinition::TEXT:
-		ret += " TEXT";
-		break;
-	default:
-		ret += " ...";
-		break;
-	}
-	return ret;
+    string ret(col->name);
+    switch (col->type) {
+        case ColumnDefinition::DOUBLE:
+            ret += " DOUBLE";
+            break;
+        case ColumnDefinition::INT:
+            ret += " INT";
+            break;
+        case ColumnDefinition::TEXT:
+            ret += " TEXT";
+            break;
+        default:
+            ret += " ...";
+            break;
+    }
+    return ret;
 }
 
 /**
@@ -183,18 +184,18 @@ string columnDefinitionToString(const ColumnDefinition *col) {
  * @returns     a string (for now) of the SQL statment
  */
 string executeSelect(const SelectStatement *stmt) {
-	string ret("SELECT ");
-	bool doComma = false;
-	for (Expr* expr : *stmt->selectList) {
-		if(doComma)
-			ret += ", ";
-		ret += expressionToString(expr);
-		doComma = true;
-	}
-	ret += " FROM " + tableRefInfoToString(stmt->fromTable);
-	if (stmt->whereClause != NULL)
-		ret += " WHERE " + expressionToString(stmt->whereClause);
-	return ret;
+    string ret("SELECT ");
+    bool doComma = false;
+    for (Expr *expr : *stmt->selectList) {
+        if (doComma)
+            ret += ", ";
+        ret += expressionToString(expr);
+        doComma = true;
+    }
+    ret += " FROM " + tableRefInfoToString(stmt->fromTable);
+    if (stmt->whereClause != NULL)
+        ret += " WHERE " + expressionToString(stmt->whereClause);
+    return ret;
 }
 
 /**
@@ -203,7 +204,7 @@ string executeSelect(const SelectStatement *stmt) {
  * @returns     a string (for now) of the SQL statment
  */
 string executeInsert(const InsertStatement *stmt) {
-	return "INSERT ...";
+    return "INSERT ...";
 }
 
 /**
@@ -212,21 +213,21 @@ string executeInsert(const InsertStatement *stmt) {
  * @returns     a string (for now) of the SQL statment
  */
 string executeCreate(const CreateStatement *stmt) {
-	string ret("CREATE TABLE ");
-	if (stmt->type != CreateStatement::kTable )
-		return ret + "...";
-	if (stmt->ifNotExists)
-		ret += "IF NOT EXISTS ";
-	ret += string(stmt->tableName) + " (";
-	bool doComma = false;
-	for (ColumnDefinition *col : *stmt->columns) {
-		if(doComma)
-			ret += ", ";
-		ret += columnDefinitionToString(col);
-		doComma = true;
-	}
-	ret += ")";
-	return ret;
+    string ret("CREATE TABLE ");
+    if (stmt->type != CreateStatement::kTable)
+        return ret + "...";
+    if (stmt->ifNotExists)
+        ret += "IF NOT EXISTS ";
+    ret += string(stmt->tableName) + " (";
+    bool doComma = false;
+    for (ColumnDefinition *col : *stmt->columns) {
+        if (doComma)
+            ret += ", ";
+        ret += columnDefinitionToString(col);
+        doComma = true;
+    }
+    ret += ")";
+    return ret;
 }
 
 /**
@@ -235,16 +236,16 @@ string executeCreate(const CreateStatement *stmt) {
  * @returns     a string (for now) of the SQL statment
  */
 string execute(const SQLStatement *stmt) {
-	switch (stmt->type()) {
-	case kStmtSelect:
-		return executeSelect((const SelectStatement*) stmt);
-	case kStmtInsert:
-		return executeInsert((const InsertStatement*) stmt);
-	case kStmtCreate:
-		return executeCreate((const CreateStatement*) stmt);
-	default:
-		return "Not implemented";
-	}
+    switch (stmt->type()) {
+        case kStmtSelect:
+            return executeSelect((const SelectStatement *) stmt);
+        case kStmtInsert:
+            return executeInsert((const InsertStatement *) stmt);
+        case kStmtCreate:
+            return executeCreate((const CreateStatement *) stmt);
+        default:
+            return "Not implemented";
+    }
 }
 
 /**
@@ -253,52 +254,52 @@ string execute(const SQLStatement *stmt) {
  */
 int main(int argc, char *argv[]) {
 
-	// Open/create the db enviroment
-	if (argc != 2) {
-		cerr << "Usage: cpsc5300: dbenvpath" << endl;
-		return 1;
-	}
-	char *envHome = argv[1];
-	cout << "(sql5300: running with database environment at " << envHome << ")" << endl;
-	DbEnv env(0U);
-	env.set_message_stream(&cout);
-	env.set_error_stream(&cerr);
-	try {
-		env.open(envHome, DB_CREATE | DB_INIT_MPOOL, 0);
-	} catch (DbException& exc) {
-		cerr << "(sql5300: " << exc.what() << ")";
-		exit(1);
-	}
-	_DB_ENV = &env;
+    // Open/create the db enviroment
+    if (argc != 2) {
+        cerr << "Usage: cpsc5300: dbenvpath" << endl;
+        return 1;
+    }
+    char *envHome = argv[1];
+    cout << "(sql5300: running with database environment at " << envHome << ")" << endl;
+    DbEnv env(0U);
+    env.set_message_stream(&cout);
+    env.set_error_stream(&cerr);
+    try {
+        env.open(envHome, DB_CREATE | DB_INIT_MPOOL, 0);
+    } catch (DbException &exc) {
+        cerr << "(sql5300: " << exc.what() << ")";
+        exit(1);
+    }
+    _DB_ENV = &env;
 
-	// Enter the SQL shell loop
-	while (true) {
-		cout << "SQL> ";
-		string query;
-		getline(cin, query);
-		if (query.length() == 0)
-			continue;  // blank line -- just skip
-		if (query == "quit")
-			break;  // only way to get out
-		if (query == "test") {
-			cout << "test_heap_storage: " << (test_heap_storage() ? "ok" : "failed") << endl;
-			continue;
-		}
+    // Enter the SQL shell loop
+    while (true) {
+        cout << "SQL> ";
+        string query;
+        getline(cin, query);
+        if (query.length() == 0)
+            continue;  // blank line -- just skip
+        if (query == "quit")
+            break;  // only way to get out
+        if (query == "test") {
+            cout << "test_heap_storage: " << (test_heap_storage() ? "ok" : "failed") << endl;
+            continue;
+        }
 
-		// use the Hyrise sql parser to get us our AST
-		SQLParserResult* result = SQLParser::parseSQLString(query);
-		if (!result->isValid()) {
-			cout << "invalid SQL: " << query << endl;
-			delete result;
-			continue;
-		}
+        // use the Hyrise sql parser to get us our AST
+        SQLParserResult *result = SQLParser::parseSQLString(query);
+        if (!result->isValid()) {
+            cout << "invalid SQL: " << query << endl;
+            delete result;
+            continue;
+        }
 
-		// execute the statement
-		for (uint i = 0; i < result->size(); ++i) {
-			cout << execute(result->getStatement(i)) << endl;
-		}
-		delete result;
-	}
-	return EXIT_SUCCESS;
+        // execute the statement
+        for (uint i = 0; i < result->size(); ++i) {
+            cout << execute(result->getStatement(i)) << endl;
+        }
+        delete result;
+    }
+    return EXIT_SUCCESS;
 }
 
